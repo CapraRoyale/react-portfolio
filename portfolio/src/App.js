2e3e503f328eddd8de5988237a2539ed0869c6a6
CapraRoyale <siege.lehman@gmail.com>
2020-05-20 20:03:17 -0700

    Add Font Awesome Icons, adjust navbar font sizing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,23 +5,26 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <Navbar bg="dark" variant="dark" sticky="top">
-        <Navbar.Brand href="#home">Siege</Navbar.Brand>
+        <Navbar.Brand href="#home"><h2><i class="fas fa-genderless"></i> Siege</h2></Navbar.Brand>
         <Nav className="mr-auto"></Nav>
         <Nav>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#home"><h5><i class="fas fa-home"></i> Home</h5></Nav.Link>
+          <Nav.Link href="#portfolio"><h5><i class="fas fa-folder-open"></i>  Portfolio</h5></Nav.Link>
+          <Nav.Link href="#contact"><h5><i class="fas fa-address-card"></i> Contact</h5></Nav.Link>
         </Nav>
       </Navbar>
+
       <Container>
         <Row>
-          <Col><img src=""></img></Col>
+          <Col> </Col>
         </Row>
       </Container>
     </div >
