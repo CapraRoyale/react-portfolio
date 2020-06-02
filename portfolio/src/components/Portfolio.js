6890f5ac12847decdcfd38eeb284a39a3d8fea94
CapraRoyale <siege.lehman@gmail.com>
2020-06-02 16:43:10 -0700

    Add href links to portfolio page

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -9,11 +9,11 @@ const Portfolio = () => (
             <Col><h3>Portfolio.js</h3></Col>
         </Row>
         <Row>
-            <Col lg={4} md={6} sm={12}><Image src="/assets/img/screenshots/hamberder.png" thumbnail></Image></Col>
-            <Col lg={4} md={6} sm={12}><Image src="/assets/img/screenshots/potatofinder.png" thumbnail></Image></Col>
-            <Col lg={4} md={6} sm={12}><Image src="/assets/img/screenshots/newsdredger.png" thumbnail></Image></Col>
-            <Col lg={4} md={6} sm={12}><Image src="/assets/img/screenshots/react-demo-click-game.png" thumbnail></Image></Col>
-            <Col lg={4} md={6} sm={12}><Image src="/assets/img/screenshots/react-review-google-books.png" thumbnail></Image></Col>
+            <Col lg={4} md={6} sm={12}><a href="https://hamberder.herokuapp.com/"><Image src="/assets/img/screenshots/hamberder.png" thumbnail></Image></a></Col>
+            <Col lg={4} md={6} sm={12}><a href="https://potatofinder.herokuapp.com/"><Image src="/assets/img/screenshots/potatofinder.png" thumbnail></Image></a></Col>
+            <Col lg={4} md={6} sm={12}><a href="https://newsdredger.herokuapp.com/"><Image src="/assets/img/screenshots/newsdredger.png" thumbnail></Image></a></Col>
+            <Col lg={4} md={6} sm={12}><a href="https://react-demo-click-game.herokuapp.com/"><Image src="/assets/img/screenshots/react-demo-click-game.png" thumbnail></Image></a></Col>
+            <Col lg={4} md={6} sm={12}><a href="https://react-review-google-books.herokuapp.com/"><Image src="/assets/img/screenshots/react-review-google-books.png" thumbnail></Image></a></Col>
         </Row>
     </Col></Row>
 );
